809a4063d52dc8f75f63564910403fe90e67acc4
Sorin C <work@csorin.com>
2022-08-21 11:41:05 +0200

    build: add readme

--- template/src/components/index.ts
+++ template/src/components/index.ts
@@ -2,7 +2,5 @@ import Icon from './Icon';
 import Touchable from './Touchable';
 import BackArrow from './BackArrow';
 import Text from './Text';
-import TextMedium from './TextMedium';
-import TextBold from './TextBold';
 
-export {Icon, Touchable, BackArrow, Text, TextMedium, TextBold};
+export {Icon, Touchable, BackArrow, Text};
